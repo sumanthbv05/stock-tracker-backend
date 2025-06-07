f520c43339192bb2f030be9b5bee71c124853d3d
Venkata Sumanth Bathina <sumanthbv05>
2025-06-07 12:51:37 +0530

    commit changes 3

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://stock-tracker-frontend-eight.vercel.app", credentials: true }));
 app.use(express.json());
 
 mongoose
